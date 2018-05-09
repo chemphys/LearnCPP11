4351b838a317ae690ee3ea7162ab17c8476948b6
Marc Riera <mrierari@ucsd.edu>
2018-05-09 10:11:45 -0700

    Added new infor about variables

---
+++ lessons/01-intro/5-badcode.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+double a=1.0;
+double b  =    2.0;
+
+
+
+double c=   a+ b;
+std::cout 
+<< 
+"The sum of "<<    a << " and " << 
+b << " is " << 
+
+
+c << std::endl;
+
+return 0;
+}
